ff2fc04850869c75df728f13a20a144cf6ad10aa
Matthew Phillips <phillips.matthew829@gmail.com>
2022-06-15 15:04:27 -0500

    commit tree d0eed7fa5a4e34180571b44eb82b2fb8e6cc306c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ApplicationRecord
+
+# Post category is either Fiction or Non-Fiction. This step requires an inclusion validator, which was not outlined in the lesson. You'll need to refer to the Rails guide
+
+validates :title, presence: true
+validates :content, length: {minimum: 250}
+validates :summary, length: {maximum: 250}
+validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+validate :clickbait?
+
+CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+]
+
+    # Custom Validations
+    def clickbait?
+        if CLICKBAIT_PATTERNS.none? {|pat| pat.match title}
+        errors.add(:title, "must be clickbait")
+        end
+    end
 end
